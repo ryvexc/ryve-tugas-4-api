77854e3be9189db022419c323ff3a5904cacf874
ryvexc <arimasword1906@gmail.com>
2023-07-30 21:59:43 +0700

    change dbname

--- src/pages/api/dosen.js
+++ src/pages/api/dosen.js
@@ -5,12 +5,12 @@ import clientPromise from "@/lib/mongodb";
 export default async function handler(req, res) {
 	try {
 		const client = await clientPromise;
-		const db = client.db("ryve_tugas_4");
+		const db = client.db("ryve_tugas_4_android");
 
 		const data = await db.collection("dosens").find({}).toArray();
 
 		return res.json(data);
 	} catch (e) {
-		console.error(e);
+		return res.json({ error: true });
 	}
 }
