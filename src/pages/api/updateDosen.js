e7bb85f92f33e229c59e10b71a86815e77872807
ryvexc <arimasword1906@gmail.com>
2023-07-31 00:16:09 +0700

    add update api

---
+++ src/pages/api/updateDosen.js
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import clientPromise from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
+
+export default async function handler(req, res) {
+	try {
+		const client = await clientPromise;
+		const db = client.db("ryve_tugas_4_android");
+
+		const data = await db.collection("dosen").updateOne(
+			{ _id: new ObjectId(req.query.id) },
+			{
+				$set: {
+					nama: req.query.nama,
+					kelas: req.query.kelas,
+				},
+			},
+		);
+		return res.status(200).json({ ok: true });
+	} catch (e) {
+		return res.status(400).json({ error: true });
+	}
+}
