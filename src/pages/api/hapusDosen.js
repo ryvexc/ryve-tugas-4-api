8431411123433fd6cebfc1f27b9e7ae1e192de27
ryvexc <arimasword1906@gmail.com>
2023-07-31 00:00:53 +0700

    add delete api

---
+++ src/pages/api/hapusDosen.js
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import clientPromise from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
+
+export default async function handler(req, res) {
+	try {
+		const client = await clientPromise;
+		const db = client.db("ryve_tugas_4_android");
+
+		const data = await db
+			.collection("dosen")
+			.deleteOne({ id: new ObjectId(req.query.id) });
+
+		return res.status(200);
+	} catch (e) {
+		return res.status(400).json({ error: true });
+	}
+}
